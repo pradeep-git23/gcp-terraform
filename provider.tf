176040f657850a0d1d0510f292cc99a15611878e
Pradeep rawat <65698353+pradeep-git23@users.noreply.github.com>
2023-04-28 23:44:51 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -13,6 +13,10 @@ terraform {
 
  # required_version = "~> 0.15"
   required_version = "~> v1.4.2"
+ backend "gcs" {
+   bucket  = "tfstatefile"
+   prefix  = "{vvar.loadbalancer_name}.tfstate"
+ }
 }
 provider "google" {
   project = var.project
