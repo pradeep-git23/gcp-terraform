cc7c547abb938b81c99e430a205755520fb5ecee
Pradeep rawat <65698353+pradeep-git23@users.noreply.github.com>
2023-05-08 17:33:31 +0530

    Delete ssl.tf

--- ssl.tf
+++
@@ -1,7 +0,0 @@
-resource "google_compute_managed_ssl_certificate" "ssl" {
-   provider = google-beta
-   name     = "${var.loadbalancer_name}-cert"
-   managed {
-    domains = [var.domain_name]
-  }
-}
