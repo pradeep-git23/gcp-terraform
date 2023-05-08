a190f1099a0ba5853ad4e08c130a7adc246d7dd9
Pradeep rawat <65698353+pradeep-git23@users.noreply.github.com>
2023-05-08 21:35:11 +0530

    Update redis.tf

--- redis.tf
+++ redis.tf
@@ -1,16 +1,11 @@
 resource "google_redis_instance" "redis" {
   name               = "my-redis-instance"
-  tier               = "STANDARD_HA"
+  tier               = "BASIC"
   memory_size_gb     = 1
   authorized_network = "default"
   redis_version      = "REDIS_6_0"
 
   # Connect instance to instance groups
   connect_mode = "DIRECT_PEERING"
-  
-  # Instance managed group 1
-  redis_configs {
-    key   = "instance-group-1"
-    value = google_compute_instance_group_manager.ba-instance-grp-manager.self_link
-  }
+
 }
