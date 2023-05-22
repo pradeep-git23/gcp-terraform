1406287e51fe74599de4eba74a9dde6a489582b2
Pradeep rawat <65698353+pradeep-git23@users.noreply.github.com>
2023-05-22 22:26:04 +0530

    Create db.tf

---
+++ workover-analytics/db.tf
@@ -0,0 +1,49 @@
+resource "google_sql_database_instance" "postgres_instance" {
+  name             = "${var.dbinstancename}"
+  database_version = "${var.dbversion}"
+
+  settings {
+    tier = "db-f1-micro"
+	ip_configuration {
+      authorized_networks {
+        name            = "default"
+        value           = "10.128.0.0/20"
+      }
+    }
+  }
+}
+
+resource "google_sql_database" "my_database" {
+  name     = "${var.dbname}"
+  instance = google_sql_database_instance.postgres_instance.name
+}
+
+resource "google_sql_user" "my_user" {
+  name     = "${var.username}"
+  password = "${var.password}"
+  instance = google_sql_database_instance.postgres_instance.name
+
+  depends_on = [google_sql_database.my_database]
+}
+
+##Variable
+variable "dbinstancename" {
+  description = "Name of the PostgreSQL database instance"
+  default     = "postgres-instance"
+}
+variable "dbversion" {
+  description = "Version of PostgreSQL database"
+  default     = "POSTGRES_13"
+}
+variable "dbname" {
+  description = "Name of the PostgreSQL database"
+  default     = "PostgreSQL-database"
+}
+variable "username" {
+  description = "Username for the PostgreSQL user"
+  default     = "mekidb"
+}
+variable "password" {
+  description = "Password for the PostgreSQL user"
+  default     = "$tr0ng123"
+}
