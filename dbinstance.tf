e484f2c7153a4ac4a497716304937fde6554104a
Pradeep rawat <65698353+pradeep-git23@users.noreply.github.com>
2023-05-25 22:16:18 +0530

    Update dbinstance.tf

--- dbinstance.tf
+++ dbinstance.tf
@@ -1,3 +1,4 @@
+##terraform block to create firewall for db instance
 resource "google_compute_firewall" "firewall" {
   name    = "${var.dbfirewall}"
   network = "default"
@@ -5,29 +6,36 @@ resource "google_compute_firewall" "firewall" {
     protocol = "tcp"
     ports    = ["5432"]
   }
-  source_ranges = ["0.0.0.0/0"] #
+  source_ranges = ["0.0.0.0/0"]
   target_tags   = ["dbfirewall"]
 }
 
-resource "google_compute_instance" "dev" {
-  count        = {{count | int}}
-  name         = "{{hostname}}${count.index}"
-  machine_type = "{{machine_type}}"
-  #zone         = "{{region}}-a"
-  zone          = "{{zone}}"
-  tags         = {{tags | union(["created-by","concierto",]) | replace("'",'"') | replace("u",'')}}
+## Terraform block for db instance provisioning
+resource "google_compute_instance" "db_instance" {
+  name         = "${var.name}"
+  machine_type = "${var.type}"
+  zone         = "us-central1-a"
+
+  tags = ["dbfirewall"]
+
   boot_disk {
     initialize_params {
-      image = "{{image}}"
-    }
+      image = "${var.image}"
+     }
+  }
+  metadata_startup_script = "${file("./startup/script.sh")}"
+
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    email  = "secret-manager-account@testing-project-371619.iam.gserviceaccount.com"
+    scopes = ["cloud-platform"]
   }
   network_interface {
-    network = google_compute_network.vpc_network.id
-    subnetwork = google_compute_subnetwork.network-with-private-secondary-ip-ranges.id
-    access_config {
-        nat_ip = google_compute_address.static[count.index].address
-     }
-    }
+    network = "default"
+
+ #   access_config {
+ #     // Ephemeral public IP (will be used when you want to give static ip to instance)
+ #   }
+  }
   depends_on = [ google_compute_firewall.firewall]
 }
-
